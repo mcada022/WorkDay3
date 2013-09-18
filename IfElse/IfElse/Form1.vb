f7ea507c98e84f0bfdf39cc2658c5259fda400f3
idsc3101 <idsc3101@gmail.com>
2013-09-18 15:25:43 -0500

    solved

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,6 +3,21 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        Dim num1, num2, largNum As Double
+        num1 = CDbl(txtFirstNum.Text)
+        num2 = CDbl(txtSecondNum.Text)
 
+        If (num1 > num2) Then
+            largNum = num1
+            txtResult.Text = CStr(largNum)
+        ElseIf (num1 < num2) Then
+            largNum = num2
+            txtResult.Text = CStr(largNum)
+        Else
+
+
+            txtResult.Text = "The two are equal."
+
+        End If
     End Sub
 End Class
